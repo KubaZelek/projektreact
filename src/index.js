dd8d298a85cd6253df92a52adcf6ab40f52eb7a4
Zelek <jakubzelechowski.zelek@gmail.com>
2023-10-03 19:38:45 +0200

    Ogarniecie node'a na kompie w domu, dodanie routeow

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Home from './Home';
+import Header from './header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header/>
     <App />
-    <Home/>
+   
   </React.StrictMode>
 );
 
